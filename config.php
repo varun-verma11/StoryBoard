cfcf8cb152ad374cea772c9bf7a0baae0d86e6c0
Mihai <mmj11@imperial.ac.uk>
2013-06-10 16:18:16 +0100

    Adding remote DB connection to DOC

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost"; 
-$user = "postgres"; 
-$pass = "password"; 
-$db = "mydb"; 
+$host = "db.doc.ic.ac.uk"; 
+$user = "g1227124_u"; 
+$pass = "WfV6cdXbVh"; 
+$db = "g1227124_u"; 
 
 $conn = pg_connect("host=$host dbname=$db user=$user password=$pass")
     or die ("Could not connect to server\n"); 
