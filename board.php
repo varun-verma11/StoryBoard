afb9f8a07481b7f8f696f9578279a1f1de0bac75
Rohan M <rm2611@imperial.ac.uk>
2013-06-18 15:36:43 +0100

    fb commenting

--- board.php
+++ board.php
@@ -57,6 +57,8 @@
 		echo  '</b><br />';
 		storyboard($name, $npages);
 
+
+
 		echo '<script> function open_editor()'
 			. '{' 
 				. ' var lastImage = document.getElementById("lastImage");' 
@@ -94,6 +96,7 @@
 		echo '<div id="adminsection">';
 		echo '<input type=\'button\' value=\'Add new page\' onclick="add_page()" />';
 		echo '<input type=\'button\' value=\'Delete last page\' onclick="delete_page()" />';
+		echo '<div class="fb-comments" data-href="http://example.com" data-width="600" data-num-posts="10"></div>'';
 		#echo '<a href=\'board.php?name=' . $name . '&addpage=true\'>Add Page</a><br />';
 		#echo '<a href=\'board.php?name=' . $name . '&delete=true\'>Delete</a>';
 		echo '</div>';
