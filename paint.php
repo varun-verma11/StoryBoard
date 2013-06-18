0778251a9b42acbafded7f98775750c38d4fa5a8
Varun Verma <vv311@ic.ac.uk>
2013-06-18 21:34:27 +0100

    made paint script catch error better now. All the error cases dont let the js be returned and the user is redirected to the home page

--- paint.php
+++ paint.php
@@ -1,71 +1,64 @@
-<!DOCTYPE html>
+<?php 
+    include_once('functions.php');
+    $board = $_GET['b'];
+    $frame = $_GET['f'];
+    $filepath = "./storyboard/" . $board . "/" . $frame . ".png";
+    $age = time()-filemtime($filepath);
 
-<html>
-	<head>
-        <meta charset="UTF-8">
-    	<link rel="stylesheet" href="./SkyBrush/skybrush/css/skybrush.css" />
-        <?php
-            header('X-Frame-Options: GOFORIT'); 
-            $board = $_GET['b'];
-            $frame = $_GET['f'];
-            $filepath = "./storyboard/" . $board . "/" . $frame . ".png";
-            $age = time()-filemtime($filepath);
+    if (!has_cookies())
+    {
+        echo '<meta http-equiv="refresh" content="0; url=./index.php">';
+        echo '<script> alert("Please log in before you can use the paint application."); </script>';
+    } else if (!file_exists($filepath))
+    {
+        echo '<meta http-equiv="refresh" content="0; url=./index.php">';
+        echo '<script> alert("You cannot access the images directly. Please log in first."); </script>';
+    } else if ($age<6)
+    {
+        echo '<meta http-equiv="refresh" content="0; url=./concurrent_access.html">';
+         echo '<script> alert("One of your group member is currently editing this image. Please try and edit later."); </script>';
+    } else 
+    {
+        echo '<html>'
+            . '<head>'
+            . '<meta charset="UTF-8">'
+            .  '<link rel="stylesheet" href="./SkyBrush/skybrush/css/skybrush.css" />'
+            . '</head>'
+            . '<body>'
+            . '<style>'
+            .   ' html, body { '
+                .   ' width: 100%;'
+                .   ' height: 100%;'
+                .   ' padding: 0;'
+                .   ' margin: 0;'
+            .   '}'
+            . '</style>'
+            . '<div class="skybrush"></div>'
+            . '<script src="./SkyBrush/skybrush/js/skybrush.js"></script>'
+            . '<script src="./SkyBrush/skybrush/js/jquery-1.8.2.min.js"></script>'
+            . '<script src="./SkyBrush/skybrush/js/jquery.more.js"></script>'
+            . '<script> var filepath="' . $filepath . '";'
+                . 'var dom = $( ".skybrush" );'
+                . 'var skybrush = new SkyBrush( dom, {'
+                    . 'image_location: "./SkyBrush/skybrush/images/skybrush/"'
+                    . '});'
+                . 'var img  = new Image();'
+                . 'img.src = filepath;'
+                . 'skybrush.setImage(img);'
+                . 'jQuery(window).bind('
+                    . '"beforeunload", '
+                    . 'function() { '
+                    . 'saveCanvas();'
+                    .   '}'
+                . ');'
+                . 'saveCanvas();'
+                . '$(document).ready(function () {'
+                    . 'window.setInterval(saveCanvas, 5000);'
+                . '});'
+            . '</script>'
+            . '<script src="./paint.js"> </script>'
+            . '</body>'
+            . '</html>';
 
-            if (!file_exists($filepath))
-            {
-                echo '<meta http-equiv="refresh" content="0; url=./NonExistentFrame.html">';
-            }  else if ($age<6)
-            {
-                echo '<meta http-equiv="refresh" content="0; url=./concurrent_access.html">';
-            }
-        ?>
-    </head>
-    <style>
-        html, body {
-            width: 100%;
-            height: 100%;
-            padding: 0;
-            margin: 0;
-            /*overflow: hidden;*/
-        }
-    </style>
-    <body>
-    	<div class="skybrush"></div>
-
-        <script src="./SkyBrush/skybrush/js/skybrush.js"></script>
-        <script src="./SkyBrush/skybrush/js/jquery-1.8.2.min.js"></script>
-        <script src="./SkyBrush/skybrush/js/jquery.more.js"></script>
-
-        <script>
-            var dom = $( '.skybrush' );
-            var skybrush = new SkyBrush( dom, {
-                image_location: './SkyBrush/skybrush/images/skybrush/'
-            });
-            var filepath = "<?php echo $filepath ?>";
-            var img  = new Image();
-            img.src = filepath;
-            skybrush.setImage(img);
-
-            jQuery(window).bind(
-                "beforeunload", 
-                function() { 
-                    saveCanvas();
-                }
-            );
-            saveCanvas();
-            $(document).ready(function () {
-                window.setInterval(saveCanvas, 5000);
-            });
-
-            function saveCanvas()
-            {
-                var img = skybrush.getImageData("image/png");
-                var ajax = new XMLHttpRequest();
-                img = filepath + "#" + img;
-                ajax.open("POST", './saveImage.php', false);
-                ajax.setRequestHeader('Content-Type', 'application/upload');
-                ajax.send(img);
-            };
-        </script>
-    </body>
-</html>
+    }
+?>
